17f42185105dd94dab70d97ed6f8a97cf586079b
otitoju1 <john.o@allprimetech.com>
2022-09-17 00:49:19 +0100

    create nft card component

---
+++ components/SubInfo.js
@@ -0,0 +1,88 @@
+import { View, Text, Image } from "react-native";
+import { assets, COLORS, FONTS, SHADOWS, SIZES } from "../constants";
+
+export const NFTTitle = () => {
+  return (
+    <View>
+      <Text>NFTTitle</Text>
+    </View>
+  );
+};
+
+export const ETHPrice = () => {
+  return (
+    <View>
+      <Text>ETHPrice</Text>
+    </View>
+  );
+};
+
+export const ImageCmp = ({ imgUrl, index}) => {
+  return (
+    <Image 
+    source={imgUrl}
+    resizeMode="contain"
+    style={{
+        width: 48,
+        height: 48,
+        marginLeft: index === 0 ? 0 : -SIZES.font
+    }}
+    />
+  );
+};
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      {[assets.person02, assets.person03, assets.person04].map((imgUrl, index) => (
+        <ImageCmp imgUrl={imgUrl} index={index} key={`People-${index}`} />
+      ))}
+    </View>
+  );
+};
+
+export const EndDate = () => {
+  return (
+    <View style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.font,
+        backgroundColor: COLORS.white,
+        ...SHADOWS.light,
+        justifyContent: 'center',
+        alignItems: 'center',
+        elevation: 1,
+        maxWidth: "50%"
+    }}>
+      <Text style={{
+        fontFamily: FONTS.regular,
+        fontSize: SIZES.small,
+        color: COLORS.primary
+      }}>
+        Ending in
+      </Text>
+
+      <Text style={{
+        fontFamily: FONTS.semiBold,
+        fontSize: SIZES.medium,
+        color: COLORS.primary
+      }}>
+        9h 45m
+      </Text>
+    </View>
+  );
+};
+
+export const SubInfo = () => {
+  return (
+    <View style={{
+        width: "100%",
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: "row",
+        justifyContent: "space-between"
+    }}>
+      <People />
+      <EndDate />
+    </View>
+  );
+};
