0c29f9417cc9ecd243ae4348905147b6f2fa4687
otitoju1 <john.o@allprimetech.com>
2022-09-18 01:50:06 +0100

    closed details

--- components/Button.js
+++ components/Button.js
@@ -1,42 +1,55 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import { COLORS, SHADOWS, SIZES } from '../constants'
+import React from "react";
+import { TouchableOpacity, Text, Image } from "react-native";
 
-export const CircleButton = ({ imgUrl, handlePress, props }) => {
+import { COLORS, SIZES, FONTS, SHADOWS } from "../constants";
+
+export const CircleButton = ({ imgUrl, handlePress, ...props }) => {
   return (
-    <TouchableOpacity style={{
+    <TouchableOpacity
+      style={{
         width: 40,
         height: 40,
         backgroundColor: COLORS.white,
-        position: 'absolute',
+        position: "absolute",
         borderRadius: SIZES.extraLarge,
-        alignItems: 'center',
-        justifyContent: 'center',
-        top: 10,
-        right: 10,
+        alignItems: "center",
+        justifyContent: "center",
         ...SHADOWS.light,
         ...props,
-    }}
-    onPress={handlePress}
+      }}
+      onPress={handlePress}
     >
-        <Image 
-            source={imgUrl}
-            resizeMode="contain"
-            style={{
-                width: 24,
-                height: 24
-            }}
-        />
+      <Image
+        source={imgUrl}
+        resizeMode="contain"
+        style={{ width: 24, height: 24 }}
+      />
     </TouchableOpacity>
-  )
-}
+  );
+};
 
-
-export const RectButton = () => {
-    return (
-      <View>
-        <Text>RecButton</Text>
-      </View>
-    )
-  }
-  
+export const RectButton = ({ minWidth, fontSize, handlePress, ...props }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        backgroundColor: COLORS.primary,
+        padding: SIZES.small,
+        borderRadius: SIZES.extraLarge,
+        minWidth: minWidth,
+        ...props,
+      }}
+      onPress={handlePress}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: fontSize,
+          color: COLORS.white,
+          textAlign: "center",
+        }}
+      >
+        Place a bid
+      </Text>
+    </TouchableOpacity>
+  );
+};
--- components/Index.js
+++ components/Index.js
@@ -1,8 +1,22 @@
-import FocusedStatusBar from "./FocusedStatusBar";
 import HomeHeader from "./HomeHeader";
 import NFTCard from "./NFTCard";
-import { CircleButton, RectButton } from "./Button";
+import { RectButton, CircleButton } from "./Button";
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+import DetailsBid from "./DetailsBid";
+import FocusedStatusBar from "./FocusedStatusBar";
+import DetailsDesc from './DetailsDesc';
 
 export {
-    FocusedStatusBar, HomeHeader, NFTCard, CircleButton, RectButton
-}
+  HomeHeader,
+  NFTCard,
+  SubInfo,
+  People,
+  EndDate,
+  RectButton,
+  CircleButton,
+  EthPrice,
+  NFTTitle,
+  DetailsBid,
+  FocusedStatusBar,
+  DetailsDesc,
+};
